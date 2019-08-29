a15d3edf6652375d819c0d2446c268149dfa0ffe
cmw44 <54688941+cmw44@users.noreply.github.com>
2019-08-29 12:16:16 -1000

    Homework 1
    Write a simple Python program that uses the print function to print the contents of what would be a simple HTML document.

---
+++ hw1.py
@@ -0,0 +1,24 @@
+# hw1.py Cameron Wertelka
+out = """<html>
+    <head>
+        <title>Jane Doe's hobbies</title>
+    <head>
+    <body>
+        <h1>My Hobbies</h1>
+        <table border="1">
+            <tr>
+                <th>Hobby</th> <th>Number of Years</th>
+            </tr>
+            <tr>
+                <th>Reading Books</th> <th>50+</th>
+            </tr>
+            <tr>
+                <th>Computer Programming</th> <th> 25</th>
+            </tr>
+            <tr>
+                <th>Building Computers</th> <th>2</th>
+            </tr>
+        </table>
+    </body>
+</html>"""
+print(out)

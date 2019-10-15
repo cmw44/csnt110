682b71ae14d24c80ecf16b2beaf895e63536ac2d
cmw44 <54688941+cmw44@users.noreply.github.com>
2019-10-15 04:07:46 -1000

    Project 1
    For this project, you will be writing a program that writes to an XML file. The XML file will be
    used to store some data for students. There are some elements in the XML file that are
    optional,so not every <student> element will have the optional elements.

--- proj1.py
+++ proj1.py
@@ -6,12 +6,13 @@ Write '<?xml version="1.0" ?>\n' to outfile
 Write '<students>\n' to outfile
 Set done to false
 While not done
-    Ask to enter a student (y/n), store as response 
+    Ask to enter a student (y/n), store as response
     Strip response, force response to lowercase
     If reponse is 'n'
     Set done to True
 Write '</students>' to outfile
 Close outfile
+https://stackoverflow.com/questions/1995615/how-can-i-format-a-decimal-to-always-show-2-decimal-places
 """
 
 #filename = input("Enter name of XML file to write to: ")
@@ -32,14 +33,36 @@ while (not done):
         major = major.strip().upper()
         status = input("Is student full-time? (y/n)? ")
         status = status.strip().lower()
-        if (status == 'n'):
-            credits = input("Enter number of credits for part-time: ")
-        elif (status == 'y'):
-        outfile.write(' <student>\n')
-        outfile.write('     <name>' + name + '</name>\n')
-        outfile.write('     <major>' + major + '</major>\n')
-        outfile.write('     <status>' + status + '</status>\n')
-        outfile.write('     <credits>' + credits + '</credits>\n')
-        outfile.write(' </student>\n')
+        if (status == 'y'):
+            status = ("full-time")
+            credit = ("-")
+            outfile.write(' <student>\n')
+            outfile.write('     <name>' + name + '</name>\n')
+            outfile.write('     <major>' + major + '</major>\n')
+            outfile.write('     <status>' + status + '</status>\n')
+            outfile.write('     <credit>' + credit + '</credit>\n')
+            outfile.write(' </student>\n')
+        elif (status == 'n'):
+            status = ("part-time")
+            def cred(message):
+                while True:
+                    try:
+                        userInput = float(input(message))
+                    except ValueError:
+                        print("Please input an integer. Try again.")
+                        continue
+                    else:
+                        userInput = round(userInput,2)
+                        return userInput
+                        continue
+            credit = cred("Enter number of credits for part-time: ")
+            outfile.write(' <student>\n')
+            outfile.write('     <name>' + name + '</name>\n')
+            outfile.write('     <major>' + major + '</major>\n')
+            outfile.write('     <status>' + status + '</status>\n')
+            outfile.write('     <credit>' + str(credit) + '</credit>\n')
+            outfile.write(' </student>\n')
+            
 outfile.write('</students>')
 outfile.close()
+
